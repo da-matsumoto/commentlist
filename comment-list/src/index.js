f30694907b985fda73e56425d11698a0ba784bc1
da-matsumoto <da.mats1990@gmail.com>
2018-07-03 22:50:53 +0900

    first commit

---
+++ comment-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function SlideTittle () {
+    return <h1>ReactでHello, world</h1>
+}
+
+ReactDOM.render(<SlideTittle />, document.getElementById('root'));
+
